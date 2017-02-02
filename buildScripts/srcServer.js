c3a882234e2c9dfda7083d1d2b49339aa3bc4822
bakinnear <barrykinnear@gmail.com>
2017-02-02 01:16:43 +0000

    Plug in mock api using fetch

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,15 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'))
 });
 
+app.get('/users/', function(req, res) {
+  // Hard coding for simplicity. Pretend this is a real database.
+  res.json([
+    {"id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com"},
+    {"id": 2, "firstName": "Ben", "lastName": "Dover", "email": "ben@gmail.com"},
+    {"id": 3, "firstName": "Irene", "lastName": "Dover", "email": "irene@gmail.com"}
+  ]);
+});
+
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
